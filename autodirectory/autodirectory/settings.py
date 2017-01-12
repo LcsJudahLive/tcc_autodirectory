5c85f522c97e21d90804001f1e333bd946287b0a
Lucas Santos <lucassantosdosanjos@yahoo.com>
2017-01-12 00:09:36 -0200

    Adding SemanticUI and Jquery, wiriting first views to render

--- autodirectory/autodirectory/settings.py
+++ autodirectory/autodirectory/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'tcc_autodirectory.apps.TccAutodirectoryConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- autodirectory/tcc_autodirectory/views.py
+++ autodirectory/tcc_autodirectory/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-	return HttpResponse("That's the first page of our TCC!")
+	return render(request,'tcc_autodirectory/index.html')
